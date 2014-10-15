f4754b702953404b269a0c3e8b36f5e0ae11f7a1
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2014-10-15 19:12:48 +0200

    add a http server that dumps everything it receives, for debuggin

---
+++ testserver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("GET path: " + self.path)
+
+    def do_POST(self):
+        print("POST")
+        print("path: " + self.path)
+        print("headers:")
+        for header in self.headers._headers:
+            print(header)
+        print("body: " + str(self.rfile.read(int(self.headers.get('content-length')))))
+
+serber = HTTPServer(('127.0.0.1', 8080), Handler)
+serber.serve_forever(0.5)
